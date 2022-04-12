5d1142781f0ba28a0657fbcb3c19a07e877e5000
Roman Ashikov <rmn.shkv@gmail.com>
2022-04-13 01:04:25 +0700

    add form and bind inputs to models

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,46 @@
 <template>
-  <div>
-    <div class="post">
-      <div><strong>Название:</strong> Пост о JavaScript</div>
-      <div><strong>Описание:</strong> JavaScript is awesome!</div>
+  <div class="container mb-3 mt-3">
+    <h4>Создание поста</h4>
+    <form class="row">
+      <div class="mb-3">
+        <label for="title" class="form-label w-100">
+          Название
+          <input
+            type="text"
+            class="form-control"
+            id="title"
+            placeholder="Супер-пост"
+            v-bind:value="title"
+          >
+        </label>
+      </div>
+      <div class="mb-3">
+        <label for="body" class="form-label w-100">
+          Содержимое
+          <textarea
+            class="form-control"
+            id="body"
+            rows="3"
+            placeholder="Очень крутой пост"
+            v-bind:value="body"
+          ></textarea>
+        </label>
+      </div>
+      <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+        <button type="button" class="btn btn-primary" @click="createPost">Создать</button>
+      </div>
+    </form>
+    <div
+      class="post border border-primary mt-3"
+      v-for="post in posts"
+      :key="post.id"
+    >
+      <div>
+        <strong>Название:</strong> {{ post.title }}
+      </div>
+      <div>
+        <strong>Содержимое:</strong> {{ post.body }}
+      </div>
     </div>
   </div>
 </template>
@@ -11,16 +49,19 @@
 export default {
   data() {
     return {
-      likes: 0,
-      dislikes: 0,
+      posts: [
+        { id: 1, title: 'Post 1', body: 'Body of Post 1' },
+        { id: 2, title: 'Post 2', body: 'Body of Post 2' },
+        { id: 3, title: 'Post 3', body: 'Body of Post 3' },
+        { id: 4, title: 'Post 4', body: 'Body of Post 4' },
+      ],
+      title: '',
+      body: '',
     };
   },
   methods: {
-    addLike() {
-      this.likes += 1;
-    },
-    addDislike() {
-      this.dislikes += 1;
+    createPost() {
+
     },
   },
 };
